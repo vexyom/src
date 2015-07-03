ac5065bae5e11580ab7ede1a38d75776951c41a7
vexyom <vexyom@yahoo.com>
2015-07-03 11:38:30 +0300

    vexyom

--- com/javarush/test/level05/lesson12/home04/Solution.java
+++ com/javarush/test/level05/lesson12/home04/Solution.java
@@ -4,10 +4,16 @@ package com.javarush.test.level05.lesson12.home04;
 Вывести на экран текущую дату в аналогичном виде "21 02 2014".
 */
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Solution
 {
     public static void main(String[] args)
     {
         //напишите тут ваш код
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyy");
+        System.out.println(dateFormat.format(date));
     }
 }
